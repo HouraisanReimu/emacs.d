e7c377f1b295290acc40dc9376732d88ee79b5be
Kaguya <908854677@qq.com>
2020-05-31 21:22:27 +0800

    不再使用F5作为打开配置文件的快捷键

--- lisp/init-keybindings.el
+++ lisp/init-keybindings.el
@@ -8,7 +8,7 @@
 (global-set-key (kbd "<f4>") 'open-pkg-file) 
 
 ;; 这一行代码，将函数 open-def-file 绑定到 <f2> 键上
-(global-set-key (kbd "<f5>") 'open-def-file) 
+;;(global-set-key (kbd "<f5>") 'open-def-file) 
 
 ;; 最近打开的文件
 (global-set-key "\C-x\ \C-r" 'recentf-open-files)
