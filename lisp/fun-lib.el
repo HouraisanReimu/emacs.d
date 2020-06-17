1dbfa6254c8bac603c43fc577867be35c1e08e62
HouraisanReimu <houraisan.reimu@gmail.com>
2020-06-17 12:23:44 +0800

    改进了插入时间的函数取法

--- lisp/fun-lib.el
+++ lisp/fun-lib.el
@@ -13,24 +13,34 @@
 ;; ====================
 ;; insert date and time
 
-;; (defvar current-date-time-format "%a %b %d %H:%M:%S %Z %Y"
-;;   "Format of date to insert with `insert-current-date-time' func
-;; See help of `format-time-string' for possible replacements")
+(defvar current-date-time-format "%Y-%m-%d %H:%M:%S"
+  "Format of date to insert with `insert-current-date-time' func
+See help of `format-time-string' for possible replacements")
 
 (defvar current-time-format "%H:%M:%S"
   "Format of date to insert with `insert-current-time' func.
 Note the weekly scope of the command's precision.")
 
-;; (defun insert-current-date-time ()
-;;   "insert the current date and time into current buffer.
-;; Uses `current-date-time-format' for the formatting the date/time."
-;;        (interactive)
-;;        (insert "==========\n")
-;; ;       (insert (let () (comment-start)))
-;;        (insert (format-time-string current-date-time-format (current-time)))
-;;        (insert "\n")
-;;        )
+(defvar current-date-format "%Y-%m-%d"
+  "Format of date to insert with `insert-current-time' func.
+Note the weekly scope of the command's precision.")
 
+(defun insert-current-date-time ()
+  "insert the current date and time into current buffer.
+Uses `current-date-time-format' for the formatting the date/time."
+       (interactive)
+       ;; (insert "==========\n")
+      ;; (insert (let () (comment-start)))
+       (insert (format-time-string current-date-time-format (current-time)))
+       ;; (insert "\n")
+       )
+
+(defun insert-current-date ()
+  "insert the current time (1-week scope) into the current buffer."
+       (interactive)
+       (insert (format-time-string current-date-format (current-time)))
+       ;; (insert "\n")
+       )
 (defun insert-current-time ()
   "insert the current time (1-week scope) into the current buffer."
        (interactive)
